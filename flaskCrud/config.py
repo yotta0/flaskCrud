7f09dc1bb9a17204cc7a070723ef1d983b6b9798
Davi Almeida <davi.almeida@iqnus.com.br>
2022-01-02 22:08:36 -0300

    Created application folder and Variables Configuration

---
+++ flaskCrud/config.py
@@ -0,0 +1,27 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-is-suposed-to-be-a-secret-key'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+
+
+
